803868a9d801e4af5a8acbdd7a60a050297e4188
smallDazza <smalldazza@gmail.com>
2025-02-21 18:23:02 +1100

    Env: added the routes to the server.js file

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-
+const authRoutes = require("./routes/authRoutes");
+const userRoutes= require("./routes/userRoutes");
+const giftListRoutes = require("./routes/giftListRoutes");
 
 
 const app = express();
@@ -11,7 +13,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use();
-app.use();
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/giftlist", giftListRoutes);
 
 module.exports = { app };
