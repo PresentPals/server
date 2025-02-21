5322ff3faabb2f5b5f164bbf587cdfab40118eed
smallDazza <smalldazza@gmail.com>
2025-02-21 11:26:42 +1100

    Models: changed the word item to gift, changed the childs list of gifts to a nested object and added comments

--- src/models/GiftListModel.js
+++ src/models/GiftListModel.js
@@ -1,38 +1,66 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
+
+const childGiftSchema = new mongoose.Schema({
+    // Schema list for the gifts the child would like.
+    childGift: {
+        // Gifts added the child would like.
+        type: String,
+        required: true
+      },
+      giftDescription: {
+        // Description of the gift.
+        type: String,
+      },
+      giftImage: {
+        // Image of the gift.
+        type: String
+      },
+      giftWebAddress: {
+        // Url of where gift can be purchased.
+        type: String
+      }
+});
 
 const GiftListSchema = new mongoose.Schema({
     giftListTitle: {
+        // Title of the gift list.
         type: String,
         required: true
     },
     giftListImage: {
-        type: String
+        // If the user creating wants to add a gift list image.
+        type: String,
     },
     childUser: {
+        // The child user this gift list is for.
         type: String,
         required: true
     },
-    childItem: {
-        type: String,
+    childGiftList: {
+        // Array of the childs gift objects.
+        type: [childGiftSchema],
         required: true
     },
-    itemDescription: {
-        type: String
-    },
-    itemWebAddress: {
-        type: String
+    userCreated: {
+        // User that created the list.
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     },
     dateCreated: {
+        // Date the list was created.
         type: Date,
         default: Date.now
     },
     dateExpired: {
-        type: Date
+        // Date the list expires = because of the childUsers birthdate, Christmas day, date of the party etc.
+        type: Date,
+        required: true
     },
 });
 
 const GiftList = mongoose.model("GiftList", GiftListSchema);
 
 module.exports = {
-    GiftList
+  GiftList
 };
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,51 +1,60 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        match: [/.+@.+\..+/i, 'Please enter a valid email address']
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6,
-    },
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastnname: {
-        type: String,
-        required: true
-    },
-    phonenumber: {
-        type: String,
-        required: true
-    },
-    admin: {
-        type: Boolean,
-        required: true,
-        default: true
-    },
-    avatar: {
-        type: String
-    },
-    userImage: {
-        type: String
-    },
-    age: {
-        type: Number,
-        min: 0,
-        max: 110
-    }
+  email: {
+    // The email address for a user to log in.
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/.+@.+\..+/i, "Please enter a valid email address"]
+  },
+  password: {
+    // The user password to login.
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  firstname: {
+    // Users first name.
+    type: String,
+    required: true
+  },
+  lastname: {
+    // Users last name.
+    type: String,
+    required: true
+  },
+  phonenumber: {
+    // Users phone number.
+    type: String,
+    required: true
+  },
+  admin: {
+    // If the user has admin rights or not.
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  avatar: {
+    // The user can add their own avatar image.
+    type: String
+  },
+  userImage: {
+    // The user can add a image of themselves.
+    type: String
+  },
+  age: {
+    // Age of the user.
+    type: Number,
+    min: 0,
+    max: 110
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
 
 module.exports = {
-    User
+  User
 };
