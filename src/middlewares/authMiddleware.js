50bdee960133e17ba983d2df7e3fcf2f0b24b3bb
smallDazza <smalldazza@gmail.com>
2025-02-22 12:33:41 +1100

    Auth: added the checking of token and try/catch block

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -6,11 +6,22 @@ async function validateToken (request, response, next) {
     // Get the token from the header.
     const token = request.headers.jwt;
 
+    // If no token is provided.
+    if (!token) {
+        return response.status(403).json({ message: "Forbidden access. No security token in place."});
+    }
+try {
     // Verify the token & the secret from .env file
     
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     request.authUserData = decoded;
     next();
+
+} catch (error) {
+    // If the token has expired or is invalid.
+
+    return response.status(401).json({ message: "Unauthorised access, your security token has expired !"});
+}  
 }
 
 module.exports = {
