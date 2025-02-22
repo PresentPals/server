65b6a527c676ab9aca98f0bbbc1eca6d3654e459
smallDazza <smalldazza@gmail.com>
2025-02-22 14:07:44 +1100

    Routes: added the createGiftList route and fixed the delete route

--- src/routes/giftListRoutes.js
+++ src/routes/giftListRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getGiftList, updateGiftList, deleteGiftList  } = require("../controllers/giftListController");
+const { createGiftList, getGiftList, updateGiftList, deleteGiftList  } = require("../controllers/giftListController");
 const { validateToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 //use the validateToken middleware on these routes
 router.use(validateToken);
+
+// POST localhost:3000/api/giftlist/
+router.post("/", createGiftList)
 
 // GET localhost:3000/api/giftlist/:id
 router.get("/giftlist/:id", getGiftList);
@@ -14,6 +17,6 @@ router.get("/giftlist/:id", getGiftList);
 router.patch("/giftlist/update/:id", updateGiftList);
 
 // DELETE localhost:3000/api/giftlist/delete/:id
-router.patch("/giftlist/delete/:id", deleteGiftList);
+router.delete("/giftlist/delete/:id", deleteGiftList);
 
 module.exports = router;
