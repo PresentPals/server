2dce64d36b4e3f128dc43b3398bbc041b3adf3d9
Venita Kissell <venitak@hotmail.com>
2025-03-01 20:48:00 +1000

    feat: updated server6_DB code with explanation quotes.

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,18 +1,21 @@
+// Import mongoose for MongoDB
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Get the database URL from the environment file
 async function connectDB() {
-    let databaseUrl = process.env.DATABASE_URL;
+  let databaseUrl = process.env.DATABASE_URL;
 
-    try {
-        await mongoose.connect(databaseUrl);
+  // Try to connect to MongoDB using the URL from .env file
+  try {
+    await mongoose.connect(databaseUrl);
+    // Log to the console successful connection
     console.log("Connected to the database !");
-
-    } catch (error) {
-        console.error("Error connecting to mongoDB: ", error);
-        process.exit(1);
-    }
-    
+    // If connection fails, log error and exit process with failure code
+  } catch (error) {
+    console.error("Error connecting to mongoDB: ", error);
+    process.exit(1);
+  }
 }
 
 module.exports = { connectDB };
