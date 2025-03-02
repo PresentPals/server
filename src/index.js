63dc137d517c2efa7cddb0f9c786659550ccde2d
smallDazza <smalldazza@gmail.com>
2025-03-02 16:26:48 +1100

    Server: updated server.js with corsoptions

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { connectDB } = require("./utils/database");
 require("dotenv").config();
 
 // GET the PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async() => {
     // Server is running at this point
--- src/server.js
+++ src/server.js
@@ -1,22 +1,20 @@
-// Imports dependencies
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-// Importing Routes
 const authRoutes = require("./routes/authRoutes");
 const userRoutes= require("./routes/userRoutes");
 const giftListRoutes = require("./routes/giftListRoutes");
 
-// express application instance
+const corsoptions = { origin: ["http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5000", "http://127.0.0.1:5000" ]};
+
+
 const app = express();
 
-// adding middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsoptions));
 app.use(express.json());
 
-// Sets up Routes to handle requests
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/giftlist", giftListRoutes);
