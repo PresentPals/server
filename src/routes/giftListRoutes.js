abe9f07127bf63c0614df4abd673be7253976129
smallDazza <smalldazza@gmail.com>
2025-03-10 21:47:22 +1100

    Route: added a new route for the sharedUser

--- src/routes/giftListRoutes.js
+++ src/routes/giftListRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createGiftList, getGiftList, updateGiftList, deleteGiftList, getAllEvents, getGiftItem, updatePurchased  } = require("../controllers/giftListController");
+const { createGiftList, getGiftList, updateGiftList, deleteGiftList, getAllEvents, getGiftItem, updatePurchased, createSharedUser  } = require("../controllers/giftListController");
 const { validateToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -13,11 +13,14 @@ router.post("/event", createGiftList)
 // GET localhost:5001/api/giftlist/item/:id
 router.get("/:id/:giftId", getGiftItem);
 
-// GET localhost:5001/api/giftlist/:id/item
+// PATCH localhost:5001/api/giftlist/:id/item
 router.patch("/:id/:giftId", updatePurchased);
 
-// UPDATE localhost:5001/api/giftlist/:id/add
+// POST localhost:5001/api/giftlist/:id/add
 router.post("/:id/add", updateGiftList);
+
+// POST localhost:5001/api/giftlist/:id
+router.post("/:id", createSharedUser);
 
 // GET localhost:5001/api/giftlist/:id
 router.get("/:id", getGiftList);
