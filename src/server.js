65008f89994ea7a43ea4ea83e4708bf9fa7fb1fd
smallDazza <smalldazza@gmail.com>
2025-03-11 20:59:44 +1100

    Sever: added to the route to accomadate images and cross origin policy so images dont get blocked

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes= require("./routes/userRoutes");
 const giftListRoutes = require("./routes/giftListRoutes");
 
-const corsoptions = { origin: ["http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5001", "http://127.0.0.1:5001"], methods: ["POST", "GET", "PATCH", "DELETE"],  credentials: true };
+const corsoptions = { origin: ["http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5001", "http://127.0.0.1:5001"], methods: ["POST", "GET", "PATCH", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true };
 
 
 const app = express();
@@ -15,6 +15,11 @@ app.use(helmet());
 app.use(cors(corsoptions));
 app.use(express.json());
 
+app.use("/uploads", express.static("uploads", {
+    setHeaders: (res, path, stat) => {
+      res.set("Cross-Origin-Resource-Policy", "cross-origin"); // Allow images to be loaded from different origins
+    }
+  }));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/giftlist", giftListRoutes);
