fa5d937c390d23334a80ffcab7304ba299ee4a54
smallDazza <smalldazza@gmail.com>
2025-03-13 13:11:06 +1100

    Server: added the netlify url to  corsoptions

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes= require("./routes/userRoutes");
 const giftListRoutes = require("./routes/giftListRoutes");
 //Options added for cors to allow these url's & headers through.
-const corsoptions = { origin: ["http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5001", "http://127.0.0.1:5001"], methods: ["POST", "GET", "PATCH", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true };
+const corsoptions = { origin: ["http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5001", "http://127.0.0.1:5001", "https://presentpals.netlify.app/"], methods: ["POST", "GET", "PATCH", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true };
 
 
 const app = express();
